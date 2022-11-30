e286029fbaac8c0870308c1c78f7858f99fb682b
Nick Santini <Nicholassantini32@gmail.com>
2022-11-30 15:04:13 -0800

    Working progress on local stores

--- sql/src/load_data.sql
+++ sql/src/load_data.sql
@@ -1,32 +1,32 @@
 COPY Users
-FROM 'users.csv'
+FROM '/extra/nsant026/CS-166-Project/data/users.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE users_userID_seq RESTART 101;
 
 COPY Store
-FROM 'stores.csv'
+FROM '/extra/nsant026/CS-166-Project/data/stores.csv'
 WITH DELIMITER ',' CSV HEADER;
 
 COPY Product
-FROM 'products.csv'
+FROM '/extra/nsant026/CS-166-Project/data/products.csv'
 WITH DELIMITER ',' CSV HEADER;
 
 COPY Warehouse
-FROM 'warehouse.csv'
+FROM '/extra/nsant026/CS-166-Project/data/warehouse.csv'
 WITH DELIMITER ',' CSV HEADER;
 
 COPY Orders
-FROM 'orders.csv'
+FROM '/extra/nsant026/CS-166-Project/data/orders.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE orders_orderNumber_seq RESTART 501;
 
 
 COPY ProductSupplyRequests
-FROM 'productSupplyRequests.csv'
+FROM '/extra/nsant026/CS-166-Project/data/productSupplyRequests.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE productsupplyrequests_requestNumber_seq RESTART 11;
 
 COPY ProductUpdates
-FROM 'productUpdates.csv'
+FROM '/extra/nsant026/CS-166-Project/data/productUpdates.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE productupdates_updateNumber_seq RESTART 51;
