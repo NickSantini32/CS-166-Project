0c25a689e765d34929ad281bc36afdb734670a51
Nick Santini <Nicholassantini32@gmail.com>
2022-11-30 19:37:46 -0800

    e

--- sql/src/load_data.sql
+++ sql/src/load_data.sql
@@ -1,32 +1,32 @@
 COPY Users
-FROM 'users.csv'
+FROM '/users.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE users_userID_seq RESTART 101;
 
 COPY Store
-FROM 'stores.csv'
+FROM '/stores.csv'
 WITH DELIMITER ',' CSV HEADER;
 
 COPY Product
-FROM 'products.csv'
+FROM '/products.csv'
 WITH DELIMITER ',' CSV HEADER;
 
 COPY Warehouse
-FROM 'warehouse.csv'
+FROM '/warehouse.csv'
 WITH DELIMITER ',' CSV HEADER;
 
 COPY Orders
-FROM 'orders.csv'
+FROM '/orders.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE orders_orderNumber_seq RESTART 501;
 
 
 COPY ProductSupplyRequests
-FROM 'productSupplyRequests.csv'
+FROM '/productSupplyRequests.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE productsupplyrequests_requestNumber_seq RESTART 11;
 
 COPY ProductUpdates
-FROM 'productUpdates.csv'
+FROM '/productUpdates.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE productupdates_updateNumber_seq RESTART 51;
