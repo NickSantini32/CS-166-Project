38cecdb5ce9b51a417670e0d976749d70a6b9560
Nick Santini <Nicholassantini32@gmail.com>
2022-11-30 22:26:51 -0800

    Update Retail.java

--- java/src/Retail.java
+++ java/src/Retail.java
@@ -275,8 +275,6 @@ public class Retail {
                 System.out.println("MAIN MENU");
                 System.out.println("---------");
                 System.out.println("1. View Stores within 30 miles");
-                System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-                viewStores(esql);
                 System.out.println("2. View Product List");
                 System.out.println("3. Place a Order");
                 System.out.println("4. View 5 recent orders");
